684db192f07c3c1a3d993b3e80f0657bdc99f99a
dev <devpalwar06@gmail.com>
2023-07-19 15:40:30 +0530

    chekckpoint 1

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import data from "../Assets/data.json";
+import { animate, animations, motion } from "framer-motion";
 
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 const Work = () => {
+  const animations = {
+    div: {
+      initial: {
+        width: 0,
+        opacity: 0
+      },
+      whileInView: {
+        width: "10%",
+        opacity: 1,
+        transition: { duration: 0.5 }
+      },
+    },
+  };
+
   return (
     <>
       <div id="Work" className="work">
         <div className="heading">
           <h2>WORK</h2>
+          <motion.div {...animations.div}></motion.div>
         </div>
         <div className="projects">
           <Carousel
@@ -36,16 +52,11 @@ const Caracard = ({ items }) => {
   return (
     <div className="caraosel" key={items.title}>
       <div className="image">
-        <img
-          src={items.imgSrc}
-          alt=""
-        />
+        <img src={items.imgSrc} alt="" />
       </div>
       <div className="info">
         <h2>{items.title}</h2>
-        <p>
-          {items.description}
-        </p>
+        <p>{items.description}</p>
         <a href={items.url}>Live demo</a>
       </div>
     </div>
